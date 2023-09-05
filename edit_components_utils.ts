07a4b7bc508789031996737f3be792c08b80532b
Simon Lalonde <simlalonde@hotmail.com>
2023-09-05 12:46:40 -0400

    Update iframe tag for dashboard + increment func

--- edit_components_utils.ts
+++ edit_components_utils.ts
@@ -1,4 +1,4 @@
-function getLineAreaCharts(parentComponents: Array<HTMLDivElement>): Array<HTMLDivElement> {
+export function getLineAreaCharts(parentComponents: Array<HTMLDivElement>): Array<HTMLDivElement> {
     return parentComponents.filter(chart => {
         // Check if it contains a <path> with 'd' attribute starting with 'M' and 'L' (typical for lines)
         let hasLineOrArea: Element | null = chart.querySelector('svg path[d^="M"][d*="L"]');
@@ -9,7 +9,7 @@ function getLineAreaCharts(parentComponents: Array<HTMLDivElement>): Array<HTMLD
     });
 }
 
-function changeSizeSelectCharts(selectCharts: Array<HTMLDivElement>, newWidthPx: number, newHeightPx: number): void {
+export function changeSizeSelectCharts(selectCharts: Array<HTMLDivElement>, newWidthPx: number, newHeightPx: number): void {
     selectCharts.forEach(element => {
         if(element) {  // Check if element exists
             element.style.width = `${newWidthPx}px`;
@@ -18,28 +18,74 @@ function changeSizeSelectCharts(selectCharts: Array<HTMLDivElement>, newWidthPx:
     });
 }
 
-function selectDashboardComponents(): Array<HTMLDivElement> | void {
-    // Open edit mode for main dashboard
-    const mainDashboard = document.getElementById("dashboard") as HTMLDivElement;
+export function selectDashboardComponents(): Array<HTMLDivElement> | void {
+    // Get dashboard inside iframe container
+    const iframe: HTMLIFrameElement | null = document.querySelector('iframe');
+    if (iframe) {
+        const innerDoc: Document | null = iframe.contentDocument
 
-    if (mainDashboard && (mainDashboard.classList.length === 0 || mainDashboard.className === "")) {
-        const editOption = document.getElementById("ui-id-2") as HTMLLIElement;
-        editOption.click();
-    } else if (!mainDashboard) {
-        console.log("Could not find element with id='dashboard'");
-    }
+        if (innerDoc) {
+            const mainDashboard = innerDoc.getElementById("dashboard") as HTMLDivElement;
+            // Open edit mode for main dashboard
+            if (mainDashboard && (mainDashboard.classList.length === 0 || mainDashboard.className === "")) {
+                const editOption = innerDoc.getElementById("ui-id-2") as HTMLLIElement;
+                editOption.click();
+            } else if (!mainDashboard) {
+                console.log("Could not find element with id='dashboard'");
+            }
+            // Select dashboard components in edit mode
+            const dashboardComponents = Array.from(
+                innerDoc.getElementsByClassName(
+                    "componentWrapper componentLoading dropshadow ui-resizable ui-draggable ui-draggable-handle"
+                ) as HTMLCollectionOf<HTMLDivElement>
+            );
+            if (!dashboardComponents || dashboardComponents.length === 0) {
+                console.log("Could not get dashboardComponents in edit-mode")
+                return
+            }
             
-    // Select dashboard components in edit mode
-    const dashboardComponents = Array.from(
-        document.getElementsByClassName(
-            "componentWrapper componentLoading dropshadow ui-resizable ui-draggable ui-draggable-handle"
-        ) as HTMLCollectionOf<HTMLDivElement>
-    );
-    if (!dashboardComponents || dashboardComponents.length === 0) {
-        console.log("Could not get dashboardComponents in edit-mode")
-        return
+            return dashboardComponents
+        }
+        else {
+            console.log("Could not find innerDocument of iframe.")
+        }
+    }
+    else {
+        console.log("Could not find iframe element.")
     }
     
-    return dashboardComponents
+}
+
+export function moveUpSelectedComponents(
+    incrementValueTop: number,
+    incrementValueLeft: number,
+    dashboardComponents: Array<HTMLDivElement>
+): void {
+    // Validate increment vals
+    if (!(incrementValueTop > 0)) {
+        throw new Error("incrementValueTop must be greater than 0.")
+    }
+    if (!(incrementValueLeft > 0)) {
+        throw new Error("incrementValueLeft must be greater than 0.")
+    }
+
+    dashboardComponents.forEach(component => {
+        // Extract position style vals
+        let topValue: string = component.style.top
+        let leftValue: string = component.style.left
+
+        let topNumber: number | typeof NaN = parseInt(topValue)
+        let leftNumber: number | typeof NaN = parseInt(leftValue)
+        if (Number.isNaN(topNumber)) {
+            topNumber = 0
+        }
+        if (Number.isNaN(leftNumber)) {
+            leftNumber = 0
+        }
+
+        // Add increment value to extracted val
+        let newTopVal = (topNumber + incrementValueTop).toString() + "px"
+        let newLeftVal = (leftNumber + incrementValueLeft).toString() + "px"
+    })
 }
 
