082751302a0041ff73ce202e5fbf9c83e5fdb735
Simon Lalonde <simlalonde@hotmail.com>
2023-09-08 15:12:02 -0400

    Implement base AddCopyPasteCompo func UNTESTED

--- edit_dashboard_utils.ts
+++ edit_dashboard_utils.ts
@@ -1,9 +1,110 @@
+// import { getDocumentFromIframe, delay } from "./edit_reports_utils"
+
+// CopyPaste to run transpiled js directly in browser, no prob bob
+function delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+function getDocumentFromIframe(iframe: HTMLIFrameElement | null): Document | null {
+    if (!iframe) return null
+    return iframe.contentDocument || null
+}
+
+
+async function openDashboardEditMode(timeout: number): Promise<[HTMLIFrameElement | null, Document | null]> {
+    // Prime iframe + innerDoc
+    const iframe: HTMLIFrameElement | null = document.querySelector('iframe');
+    let innerDoc: Document | null = null
+    
+    if (iframe) {
+        innerDoc = getDocumentFromIframe(iframe)
+
+        if (innerDoc) {
+            const mainDashboard = innerDoc.getElementById("dashboard") as HTMLDivElement;
+            
+            // Open edit mode for main dashboard
+            if (mainDashboard && (mainDashboard.classList.length === 0 || mainDashboard.className === "")) {
+                const editOption = innerDoc.getElementById("ui-id-2") as HTMLLIElement;
+                editOption.click();
+                await delay(timeout)
+
+            } else if (!mainDashboard) {
+                throw new Error("Could not find element with id='dashboard'")
+            }
+        }
+    }
+    return [iframe || null, innerDoc || null]
+}
+
 // Dashboard components edit
+async function addCopyPasteComponent(
+    selectBackwardsFrom: number=11,
+    substringTitleToReplace: string,
+    substringTitleReplacement: string
+    ): Promise<void> {
+    // Open dashboard in edit mode
+    const [iframe, innerDoc] = await openDashboardEditMode(1000)
+
+    // Get last 11 component (group to copy)
+    if (!innerDoc) throw new Error("No document found inside iframe")
+    const components: NodeListOf<HTMLDivElement> = innerDoc.querySelectorAll(
+            ".componentWrapper.componentLoading.dropshadow.ui-resizable.ui-draggable.ui-draggable-handle"
+            )
+    const componentsArr = Array.from(components).slice(-selectBackwardsFrom)
+    
+    //  Find add component and apply changes buttons
+    const addComponentButton = innerDoc.getElementById("newcomponentArea") as HTMLDivElement
+    const buttons = innerDoc.querySelectorAll("button")
+    let applyChangesButton: HTMLButtonElement | null
+    buttons.forEach(button => {
+        let onClickAttr = button.getAttribute("onclick")
+        if (onClickAttr === "dashboard.componentedit_apply();return false;") {
+            applyChangesButton = button
+        }
+        else {throw new Error("Could not find ApplyChanges button")}
+
+    })
+    // Create new empty components
+    for (let i: number = 0; i< componentsArr.length; i++) {
+        // Add a new empty component and save it
+        addComponentButton.click()
+        await delay(500)
+        applyChangesButton!?.click()
+        await delay(500)
+        console.log(
+            `Created + saved empty component ${i + 1} / ${componentsArr.length}`
+        )
+    }
+    // Modify the properties based on template components
+    const newComponents: NodeListOf<HTMLDivElement> = innerDoc.querySelectorAll(
+        ".componentWrapper.componentLoading.dropshadow.ui-resizable.ui-draggable.ui-draggable-handle"
+        )
+    if (!newComponents) { throw new Error("Could not find the added components.");
+    }
+    const newCompoArr = Array.from(newComponents).slice(-selectBackwardsFrom)
+    const topIncrement: number = 1600
+    for (let i in componentsArr) {
+        // Extract title and style from template
+        let title = componentsArr[i].children[0].innerHTML.toUpperCase()
+        let newTitle = title.replace(substringTitleToReplace, substringTitleReplacement)
+        
+        // Copy styles
+        const stylesToCopy = ["min-height", "min-width", "top", "left", "height", "width"];
+        for (let propName of stylesToCopy) {
+            newCompoArr[i].style[propName as any] = componentsArr[i].style[propName as any]
+            }
+        
+        // Adjust the top position
+        let originalTop = parseInt(newCompoArr[i].style.top);
+        let newTopVal: number = originalTop + topIncrement;
+        newCompoArr[i].style.top = newTopVal.toString() + "px";
+    }
+}
+
 function selectDashboardComponents(): Array<HTMLDivElement> | void {
     // Get dashboard inside iframe container
     const iframe: HTMLIFrameElement | null = document.querySelector('iframe');
     if (iframe) {
-        const innerDoc: Document | null = iframe.contentDocument
+        const innerDoc = getDocumentFromIframe(iframe)
 
         if (innerDoc) {
             const mainDashboard = innerDoc.getElementById("dashboard") as HTMLDivElement;
